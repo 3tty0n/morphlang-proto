7e2f2410f579a38b37bd689fbe1dcee53724ffba
Yusuke Izawa <me@yizawa.com>
2024-05-07 22:27:06 +0900

    WIP: implement proc_macro

--- morph/src/lib.rs
+++ morph/src/lib.rs
@@ -2,7 +2,24 @@ mod ast;
 // use ast::{Stmt, Expr};
 
 extern crate proc_macro;
-use proc_macro::TokenStream;
 
+use proc_macro::TokenStream;
+use syn::{parse_macro_input, Ident, LitStr};
+use quote::quote;
 use lalrpop_util::lalrpop_mod;
+
 lalrpop_mod!(grammar);
+
+#[proc_macro]
+pub fn morph(input: TokenStream) -> TokenStream {
+    let input_str = parse_macro_input!(input as LitStr);
+    let input_str_val = input_str.value();
+
+    let parser = grammar::ProgramParser::new();
+    let prog = parser.parse(&input_str_val);
+
+    // TODO: convert prog into rust function
+    quote! {
+        let x = 1;
+    }.into()
+}
